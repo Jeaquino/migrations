c86520bd4b6fc7bbf5a54982dde1307c83788b74
paola moriconi <julian.eduardo.aquino@gmail.com>
2024-02-24 13:40:06 -0300

    add user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,20 +3,28 @@ const bcrypt = require("bcrypt");
 
 module.exports = {
   create: (req, res) => {
-    const { name, username, email, password, address, city, country } = req.body;
+    const { name, surname, email, password, address, city, country, phone,province } = req.body;
           db.User.create({
             name,
-            username,
+            surname,
             email,
             password: bcrypt.hashSync(password, 10),
             avatar: req.file ? req.file.name : "default.jpg",
-            rol_id: 2
+            rol_id: 2,
+            phone
           }).then((resp) => {
-            db.Address.create({ address, city, country,user_id:resp.dataValues.id });
+            db.Address.create({ address, city, country,user_id:resp.dataValues.id,province }).then(resp => {
+              db.User.findByPk(resp.dataValues.user_id, {include:{
+                association:"addresses"
+              }}).then( resp => {
+                res.send(resp)
+              })
+
+            });
           })
           .catch((err) => console.log(err));
     },
-    
+
   allUsers: (req, res) => {
     db.User.findAll({
       include: [
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {allUsers,destroy} = require("../controllers/userController");
+const {allUsers,destroy,create} = require("../controllers/userController");
 /* GET users listing. */
 router
 .get('/', (req, res, next) => {res.send('respond with a resource');})
 .get('/all', allUsers)
 .get('/rols', (req, res, next) => {res.send('respond with a resource');})
 .get('/address', (req, res, next) => {res.send('respond with a resource');})
+.post('/createUser',create)
 .get('/destroy/:id', destroy);
 
 module.exports = router;
